e953373519868d893a028faede61113225c5e720
Matthias Klan <matthias.klan@gmail.com>
2020-10-07 09:59:09 +0200

    set config from issue

--- Demo/html/index.js
+++ Demo/html/index.js
@@ -1,72 +1,60 @@
 const { init, errorCodes } = window.anylinejs;
 
 const entityMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-  '/': '&#x2F;',
-  '`': '&#x60;',
-  '=': '&#x3D;',
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&#39;",
+  "/": "&#x2F;",
+  "`": "&#x60;",
+  "=": "&#x3D;",
 };
 
 function escapeHtml(string) {
-  return String(string).replace(/[&<>"'`=\/]/g, function(s) {
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
     return entityMap[s];
   });
 }
 
 function replaceVerticalBar(string) {
-  return String(string).replace(/[|]/g, function(s) {
-    return '\n';
+  return String(string).replace(/[|]/g, function (s) {
+    return "\n";
   });
 }
 
 const viewConfig = {
-  // captureResolution: '1080p',
-  outerColor: '000000',
-  outerAlpha: 0.5,
+  outerColor: "000000",
+  outerAlpha: 0.8,
   cutouts: [
     {
       cutoutConfig: {
-        // style: 'rect',
-        maxWidthPercent: '80%',
-        alignment: 'top_half',
+        maxWidthPercent: "80%",
+        alignment: "top_half",
         ratioFromSize: {
           width: 300,
           height: 250,
         },
         width: 720,
-        strokeWidth: 2,
+        strokeWidth: 0,
         cornerRadius: 4,
-        strokeColor: 'FFFFFFFF',
-        feedbackStrokeColor: '0099FF',
+        strokeColor: "FFFFFFFF",
+        feedbackStrokeColor: "0099FF",
       },
-      // flash: {
-      //   mode: 'manual',
-      //   alignment: 'top_left',
-      // },
-      // cancelOnResult: false,
-      // "delayStartScanTime": 2000,
       scanFeedback: {
-        style: 'contour_point',
-        strokeColor: '0099FF',
-        fillColor: '300099FF',
-        strokeWidth: 2,
+        style: "contour_point",
+        strokeColor: "0099FF",
+        fillColor: "300099FF",
+        strokeWidth: 0,
         cornerRadius: 4,
-        animationDuration: 150,
-        animation: 'NONE',
-        // beepOnResult: true,
-        // vibrateOnResult: true,
-        // blinkAnimationOnResult: true,
+        animationDuration: 300,
+        animation: "NONE",
       },
     },
   ],
 };
 
-
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 let selectedPreset = undefined;
 
 function mountAnylineJS(preset) {
@@ -78,27 +66,27 @@ function mountAnylineJS(preset) {
     license: demoLicense,
     element: root,
     debugAnyline: true,
-    anylinePath: '../../anylinejs/anylinejs'
+    anylinePath: "../../anylinejs/anylinejs",
   });
 
   let modalOpen = false;
 
-  Anyline.onResult = result => {
-    console.log('Result: ', result);
+  Anyline.onResult = (result) => {
+    console.log("Result: ", result);
     if (modalOpen) return;
     // Anyline.stopScanning();
     window.Swal.fire({
-      title: 'Result',
+      title: "Result",
       html: `<div class="result">${result.result
         .map(
-          resultEl =>
+          (resultEl) =>
             `
         <div class="resultRow">
           <div class="resultLabel">${resultEl.identifier}:</div>
-          <div>${escapeHtml(replaceVerticalBar(resultEl.text)) || 'kA'}</div>
-        </div>`,
+          <div>${escapeHtml(replaceVerticalBar(resultEl.text)) || "kA"}</div>
+        </div>`
         )
-        .join(' ')}
+        .join(" ")}
       </div>`,
       showCloseButton: true,
       onBeforeOpen: () => {
@@ -111,22 +99,22 @@ function mountAnylineJS(preset) {
     });
   };
 
-  Anyline.onReport = msg => {
-    console.log('Report: ', msg);
+  Anyline.onReport = (msg) => {
+    console.log("Report: ", msg);
   };
 
-  Anyline.onDebug = msg => {
+  Anyline.onDebug = (msg) => {
     alert(JSON.stringify(msg));
   };
 
   Anyline.onError = ({ code, message }) => {
     if (code === errorCodes.WEBCAM_ERROR) {
-      console.error('webcam error: ', message);
+      console.error("webcam error: ", message);
     }
   };
 
   Anyline.onLoad = () => {
-    console.log('ANYLINE LOADED on main thread');
+    console.log("ANYLINE LOADED on main thread");
   };
 
   Anyline.startScanning();
@@ -139,4 +127,3 @@ function remountAnylineJS() {
   Anyline.dispose();
   mountAnylineJS(selectedPreset);
 }
-
